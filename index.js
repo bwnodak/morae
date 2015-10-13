e6a788d7d3f1872a91b4ab64eaa99db2a320b666
Brian W. Nelson <yonbeastie@gmail.com>
2015-10-12 23:34:55 -0500

    Complete version 1.0.0 with passing tests

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-var syllable = require('syllable');
+var Syllable = require('syllablistic'),
+    S = require('string'),
+    SYLLABLES = { ALL: 17, LINE_ONE: 5, LINE_TWO: 7, LINE_THREE: 5 };
 
 /**
- * Extract an array of haikus from a block of text 
+ * Extract an array of haikus from a block of text
  *
  * @param {string} txt
  * @return {array}
  */
 exports.find = function find(txt) {
-  return '';
+  var words = this._splitText(this._removeHtml(txt)),
+      haikus = [],
+      currentWords, line1, line2, line3;
+
+  for(var i=0; i<words.length; i++) {
+    line1 = this._crawlSyllables(words.slice(i, words.length), SYLLABLES.LINE_ONE);
+
+    if (SYLLABLES.LINE_ONE === this._getSyllables(line1.join(' '))) {
+      line2 = this._crawlSyllables(words.slice(i + line1.length, words.length), SYLLABLES.LINE_TWO);
+
+      if (SYLLABLES.LINE_TWO === this._getSyllables(line2.join(' '))) {
+        line3 = this._crawlSyllables(words.slice(i + line1.length + line2.length, words.length), SYLLABLES.LINE_THREE);
+
+        if (SYLLABLES.LINE_THREE === this._getSyllables(line3.join(' '))) {
+          haikus.push({
+            one: line1.join(' '),
+            two: line2.join(' '),
+            three: line3.join(' ')
+          });
+        }
+      }
+    }
+  }
+
+  return haikus;
 };
 
 /**
@@ -18,7 +44,9 @@ exports.find = function find(txt) {
  * @return {string}
  */
 exports._removeHtml = function _removeHtml(txt) {
-  return '';
+  txt = txt.replace(/(<([^>]+)>)/ig, ' '); // remove tags
+  txt = txt.replace(/ +(?= )/g, ''); // remove double-spaces
+  return txt.trim(); // remove leading and trailing spaces
 };
 
 /**
@@ -27,8 +55,8 @@ exports._removeHtml = function _removeHtml(txt) {
  * @param {string} txt
  * @return {string}
  */
-exports._splitText = function _sanitizeText(txt) {
-  return '';
+exports._splitText = function _splitText(txt) {
+  return txt.split(/\s+/);
 };
 
 /**
@@ -38,5 +66,29 @@ exports._splitText = function _sanitizeText(txt) {
  * @return {number}
  */
 exports._getSyllables = function _getSyllables(txt) {
-  return syllable(txt);
+  return Syllable.text(txt);
+};
+
+/**
+ * Collect a number of syllables from an array of words. Note, at least the
+ * number of requested syllables will be returned, including a word that will
+ * exceed the number.
+ *
+ * @param {array} words
+ * @param {number} num - the number of syllables to collect
+ * @return {array}
+ */
+exports._crawlSyllables = function _crawlSyllables(words, num) {
+  var count = 0,
+      arr = [];
+
+  for(var i=0; i<words.length; i++) {
+    if (count >= num) {
+      break;
+    }
+    count += this._getSyllables(words[i]);
+    arr.push(words[i]);
+  }
+
+  return arr;
 };
--- test/test.js
+++ test/test.js
@@ -1,21 +1,19 @@
-var assert = require('assert');
-var morae = require('../');
-
-console.log(morae);
+var assert = require('assert'),
+    morae = require('../');
 
 describe('Private functions', function() {
   describe('#_removeHtml()', function () {
     it('should return text without HTML and place appropriate spaces.', function () {
       assert.equal('hello world!', morae._removeHtml('<p>hello world!</p>'));
       assert.equal('And boom goes the dynamite!', morae._removeHtml('<b><i>And</i></b> boom goes<br/>the<br>dynamite!'));
-      assert.equal('This is up top and this is is down below!', morae._removeHtml('<h1>This is up top</h1><hr/><hr/><p>and this is down below</p><b>!</b>'));
+      assert.equal('This is up top and this is down below !', morae._removeHtml('<h1>This is up top</h1><hr/><hr/><p>and this is down below</p><b>!</b>'));
     });
   });
 
   describe('#_splitText()', function () {
     it('should return an array of words', function () {
-      var txt = 'And boom\n goes\n the dynamite!';
-      var arr = morae._splitText(txt);
+      var txt = 'And  boom   goes     the      dynamite!',
+          arr = morae._splitText(txt);
 
       assert.equal(5, arr.length);
       assert.equal('And', arr[0]);
@@ -24,14 +22,35 @@ describe('Private functions', function() {
   });
 
   describe('#_getSyllables()', function () {
-    it('should return a syllable count for a word', function () {
+    it('should return a syllable count for text', function () {
       assert.equal(3, morae._getSyllables('hamburger'));
-      assert.equal(1, morae._getSyllables('mmmmmmmmmmmm'));
+      assert.equal(0, morae._getSyllables('mmmmmmmmmmmm'));
       assert.equal(7, morae._getSyllables('And boom goes the dynamite!!!'));
+      assert.equal(5, morae._getSyllables('three three three three three'));
+      assert.equal(5, morae._getSyllables('two two two two two'));
+      assert.equal(5, morae._getSyllables('one one one one one'));
     });
   });
 
-  describe('Public functions', function() {
+  describe('#_crawlSyllables()', function () {
+    it('should return an array of words matching or narrowly exceeding the desired syllable count', function () {
+      var words = 'Immediately following the catastrophe, she fell headlong into the well.'.split(' '),
+          sample1 = morae._crawlSyllables(words, 6),
+          sample2 = morae._crawlSyllables(words, 10);
+
+      assert.equal(8, morae._getSyllables(sample1.join(' ')));
+      assert.equal(2, sample1.length);
+      assert.equal('following', sample1[1]);
+
+      assert.equal(12, morae._getSyllables(sample2.join(' ')));
+      assert.equal(4, sample2.length);
+      assert.equal('catastrophe,', sample2[3]);
+    });
+  });
+});
+
+describe('Public functions', function() {
+  describe('#find()', function () {
     it('should return an array of haikus', function () {
       var txt1 = 'one one one one one two two two two two two two three three three three three miracle texas!',
           txt2 = '<p>one <b>one</b> one <i>one</i> one</p><p>two two two two two two two three three three three three</p><hr/><hr><br><br/><img/><h1>miracle texas!</h1>',
@@ -39,14 +58,30 @@ describe('Private functions', function() {
           haikus2 = morae.find(txt2);
 
       assert.equal(3, haikus1.length);
-      assert.equal('one one one one one two two two two two two two three three three three three', haikus1[0]);
-      assert.equal('one one two two two two two two two three three three three three miracle', haikus1[1]);
-      assert.equal('two two two two two two two three three three three three miracle texas!', haikus1[2]);
+      assert.equal('one one one one one', haikus1[0].one);
+      assert.equal('two two two two two two two', haikus1[0].two);
+      assert.equal('three three three three three', haikus1[0].three);
 
-      assert.equal(3, haikus.length);
-      assert.equal('one one one one one two two two two two two two three three three three three', haikus2[0]);
-      assert.equal('one one two two two two two two two three three three three three miracle', haikus2[1]);
-      assert.equal('two two two two two two two three three three three three miracle texas!', haikus2[2]);
+      assert.equal('one one two two two', haikus1[1].one);
+      assert.equal('two two two two three three three', haikus1[1].two);
+      assert.equal('three three miracle', haikus1[1].three);
+
+      assert.equal('two two two two two', haikus1[2].one);
+      assert.equal('two two three three three three three', haikus1[2].two);
+      assert.equal('miracle texas!', haikus1[2].three);
+
+      assert.equal(3, haikus2.length);
+      assert.equal('one one one one one', haikus2[0].one);
+      assert.equal('two two two two two two two', haikus2[0].two);
+      assert.equal('three three three three three', haikus2[0].three);
+
+      assert.equal('one one two two two', haikus2[1].one);
+      assert.equal('two two two two three three three', haikus2[1].two);
+      assert.equal('three three miracle', haikus2[1].three);
+
+      assert.equal('two two two two two', haikus2[2].one);
+      assert.equal('two two three three three three three', haikus2[2].two);
+      assert.equal('miracle texas!', haikus2[2].three);
     });
   });
 });
